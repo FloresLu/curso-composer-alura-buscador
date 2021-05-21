ea36cc28e363f7b6665104640ee7ca61675affc4
Lucas Flores <flores.lucas@gmail.com>
2021-05-21 10:26:42 -0300

    Primeiro Commit

---
+++ buscar-cursos.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Alura\BuscadorDeCursos\Buscador;
+use Symfony\Component\DomCrawler\Crawler;
+use GuzzleHttp\Client;
+
+$client = new Client(['base_uri'=>'https://www.alura.com.br' , 'verify'=>false]); //Nega a verificação de ssl do site da alura
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso){
+    echo exibeMensagem ($curso);
+}
